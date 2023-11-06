93fdb875ccd92cd377f85884f35c51dee961c867
Minseok Choe <choems@outlook.com>
2023-11-06 20:08:56 +0900

    add test.py as playground

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import torch
-from transformers import RobertaTokenizer, RobertaModel
+from transformers import RobertaTokenizer, RobertaModel, RobertaForMaskedLM, RobertaTokenizer, pipeline, AutoTokenizer, AutoModel
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
 model = RobertaModel.from_pretrained("microsoft/codebert-base")
 model.to(device)
-
-from transformers import AutoTokenizer, AutoModel
-import torch
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
 model = AutoModel.from_pretrained("microsoft/codebert-base")
@@ -18,7 +15,6 @@ tokens = [tokenizer.cls_token] + nl_tokens + [tokenizer.sep_token] + code_tokens
 tokens_ids = tokenizer.convert_tokens_to_ids(tokens)
 context_embeddings = model(torch.tensor(tokens_ids)[None,:])[0]
 
-from transformers import RobertaForMaskedLM, RobertaTokenizer, pipeline
 
 model = RobertaForMaskedLM.from_pretrained("microsoft/codebert-base-mlm")
 tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base-mlm")
